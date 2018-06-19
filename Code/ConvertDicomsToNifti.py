1f2885004c76bb8acf5e4631988650297cf38a5f
Johnson Jacob M <johnsonj118@gmail.com>
2018-06-19 13:12:30 -0500

    Working  version of dicom loader. Converts to hdf5 instead of nifti

--- Code/ConvertDicomsToNifti.py
+++ Code/ConvertDicomsToNifti.py
@@ -30,12 +30,17 @@ def FindDirectories(mr_dir):
     cur_dir = glob.glob(os.path.join(cur_dir, "*", ""))[0]
     # find various dicom dirs
     dcm_dirs = natsorted(glob.glob(os.path.join(cur_dir, "*", "")))
-    # find T2fat dicom dir
-    T2dir = [string for string in dcm_dirs if 'T2fat' in string][-1]
-    # find pre contrast dir
-    pre_dir = [string for string in dcm_dirs if 'Pre Ax3d' in string][-1]
-    # find post-contrast dir
-    post_dir = [string for string in dcm_dirs if 'Dur Ax3d' in string][-1]
+    try:
+        # find T2fat dicom dir
+        T2dir = [string for string in dcm_dirs if 'T2fat' in string][-1]
+        # find pre contrast dir
+        pre_dir = [string for string in dcm_dirs if 'Pre Ax3d' in string][-1]
+        # find post-contrast dir
+        post_dir = [string for string in dcm_dirs if 'Dur Ax3d' in string][-1]
+    except IndexError as e:
+        T2dir = False
+        pre_dir = False
+        post_dir = False
     return T2dir,pre_dir,post_dir
 
 # Function for loading in dicoms from a given directory
@@ -49,7 +54,7 @@ def LoadDicomDir(directory):
     locs = [(float(dicm.SliceLocation)) for dicm in dicms]
     times = [(int(dicm.AcquisitionTime)) for dicm in dicms]
     # determine if multiple phases in this directory
-    post_con = np.unique(times).size>1
+    post_con = np.unique(times).size>1 or len(times)>200
     # zip up slice locations, acquisition times, and file indicies
     loctimes = list(zip(locs,times,FNinds))
     # sort by time then slice
@@ -84,32 +89,42 @@ subj_nums = [string[-4:-1] for string in subj_dirs]
 # append '/MR' directory to each
 mr_dirs = [os.path.join(dir,'MR') for dir in subj_dirs]
 
-# subject 1
-subj = 0
-# current directory
-cur_dir = mr_dirs[subj]
-cur_subj = subj_nums[subj]
-_,pre_dir,post_dir = FindDirectories(cur_dir)
+# loop over all subjects
+sag_subjs = []
+for subj in range(333,len(mr_dirs)):
+    # current directory
+    cur_dir = mr_dirs[subj]
+    cur_subj = subj_nums[subj]
+    _,pre_dir,post_dir = FindDirectories(cur_dir)
+    if not pre_dir:
+        print('Skipping subject',cur_subj)
+        sag_subjs.append(subj)
+    else:
+        print('Processing subject',cur_subj,'...')
+        #T2_ims = LoadDicomDir(T2dir)
+        pre_ims = LoadDicomDir(pre_dir)
+        post_ims = LoadDicomDir(post_dir)
+        # normalize
+        pre_ims /= np.max(pre_ims)
+        post_ims /= np.max(post_ims)
+        try:
+            comb_ims = np.concatenate((pre_ims,post_ims),axis=-1)
+        
+            # register T2 to others
+            #T2_img = ants.from_numpy(T2_ims[...,0].astype(np.float))
+            #T2_img_resamp = T2_img.resample_image((124,512,512),use_voxels=True,interp_type=0)
+            #pre_img = ants.from_numpy(pre_ims[...,0].astype(np.float))
+            #T2_tx = ants.registration(fixed=pre_img,moving=T2_img,type_of_transform='Similarity',aff_metric='mattes')
+            #reg_T2 = T2_tx['warpedmovout']
+            #T2_ims_reg = reg_T2.numpy()[...,np.newaxis]
+            #import ants
+            
+            # export to .hdf5 file
+            savepath = output_path.format(cur_subj)
+            with h5py.File(savepath, 'w') as hf:
+                hf.create_dataset("images",  data=comb_ims,dtype='f')
+        except ValueError as e:
+            print('image size mismatch')
+            print('skipping...')
 
-#T2_ims = LoadDicomDir(T2dir)
-pre_ims = LoadDicomDir(pre_dir)
-post_ims = LoadDicomDir(post_dir)
-# normalize
-pre_ims /= np.max(pre_ims)
-post_ims /= np.max(post_ims)
-
-comb_ims = np.concatenate((pre_ims,post_ims),axis=-1)
-
-# register T2 to others
-#T2_img = ants.from_numpy(T2_ims[...,0].astype(np.float))
-#T2_img_resamp = T2_img.resample_image((124,512,512),use_voxels=True,interp_type=0)
-#pre_img = ants.from_numpy(pre_ims[...,0].astype(np.float))
-#T2_tx = ants.registration(fixed=pre_img,moving=T2_img,type_of_transform='Similarity',aff_metric='mattes')
-#reg_T2 = T2_tx['warpedmovout']
-#T2_ims_reg = reg_T2.numpy()[...,np.newaxis]
-#import ants
-
-# export to .hdf5 file
-savepath = output_path.format(cur_subj)
-with h5py.File(savepath, 'w') as hf:
-    hf.create_dataset("images",  data=comb_ims,dtype='f')
+print('Done')
