99ff01d9e0eb17ee96e1dd56c38c21e0f31bc3f4
Jacob Johnson <jmjohnson33@wisc.edu>
2018-07-11 13:26:40 -0500

    Added code for converting coordinate data into YOLO target data

--- Code/CustomDataGen.py
+++ Code/CustomDataGen.py
@@ -12,7 +12,7 @@ import os
 class DataGenerator(keras.utils.Sequence):
     'Generates data for Keras'
     def __init__(self, list_IDs, labels, batch_size=32, dim=(256,256), 
-                 n_channels=1, shuffle=True):
+                 n_channels=1, shuffle=True,grid_size=(18,18)):
         'Initialization'
         self.dim = dim
         self.batch_size = batch_size
@@ -20,6 +20,7 @@ class DataGenerator(keras.utils.Sequence):
         self.list_IDs = list_IDs
         self.n_channels = n_channels
         self.shuffle = shuffle
+        self.grid_size = grid_size
         self.data_path = os.path.expanduser(
                             os.path.join('~','r-fcb-isilon',
                                      'groups','StrigelGroup',
@@ -54,7 +55,7 @@ class DataGenerator(keras.utils.Sequence):
         'Generates data containing batch_size samples' # X : (n_samples, *dim, n_channels)
         # Initialization
         X = np.empty((self.batch_size, *self.dim, self.n_channels))
-        y = np.empty((self.batch_size), dtype=int)
+        y = np.empty((self.batch_size, *self.grid_size, 7))
 
         # Generate data
         for i, ID in enumerate(list_IDs_temp):
@@ -63,14 +64,53 @@ class DataGenerator(keras.utils.Sequence):
             # load coordinates
             coords = np.loadtxt(self.data_path + ID + '.txt')
             # convert coordinates to YOLO
+            # preallocate
+            target = np.zeros((*self.grid_size,7))
             # iterate over 4 possible ROIs
             for r in [0,5,10,15]:
-                
+                cdat = coords[r:r+5]
+                if np.any(cdat):
+                    # coordinates are in format:
+                    # [x1,y1,x2,y2,b/m]
+                    # calculate center coordinates
+                    bx = (cdat[2]+cdat[0])/2
+                    by = (cdat[3]+cdat[1])/2
+                    # calculate width and height
+                    bw = cdat[2]-cdat[0]
+                    bh = cdat[3]-cdat[1]
+                    # calculate grid square width and height
+    #                gw = self.dim[0]/self.grid_size[0]
+    #                gh = self.dim[1]/self.grid_size[1]
+                    gw = dim[0]/grid_size[0]
+                    gh = dim[1]/grid_size[1]
+                    # find grid square that contains center
+                    xind = np.floor(bx/gw).astype(np.int)
+                    yind = np.floor(by/gh).astype(np.int)
+                    # find offset from grid square
+                    cx = gw*xind
+                    cy = gh*yind
+                    # calculate relative offset within square
+                    ox = (bx-cx)/gw
+                    oy = (by-cy)/gh
+                    # calculate center logits
+                    tx = np.log(ox/(1-ox))
+                    ty = np.log(oy/(1-oy))
+                    # calculate height/width logits
+                    tw = np.log(bw/gw)
+                    th = np.log(bh/gh)
+                    # class assignment
+                    pb = np.int(cdat[4]==1)
+                    pm = np.int(cdat[4]==2)
+                    # combine into vector
+                    vec = [tx,ty,tw,th,1,pb,pm]
+                    # assign to target grid
+                    target[xind,yind,:] = vec
+            
             # Store image in batch
-            X[i,] = np.load()
+            X[i,] = im
 
             # Store target in batch
-            y[i] = self.labels[ID]
+            y[i,] = target
 
         return X, y
     
