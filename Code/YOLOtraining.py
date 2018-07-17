9e7b1c4c59baa882a6bc67362625dc4108199d73
Johnson Jacob M <johnsonj118@gmail.com>
2018-07-17 10:47:27 -0500

    load validation data instead of using generator

--- Code/YOLOtraining.py
+++ Code/YOLOtraining.py
@@ -17,6 +17,7 @@ from glob import glob
 import YOLOmodels
 from VisTools import YOLOviewer
 from CustomDataGen import DataGenerator
+from YOLOtools import CoordsToTarget
 
 # Use first available GPU
 import GPUtil
@@ -38,6 +39,8 @@ data_dir = os.path.expanduser(os.path.join('~','r-fcb-isilon','groups',
                                            'StrigelGroup',
                                            'BreastCancerClassification',
                                            'ProcessedData',''))
+im_dim = (384,384)
+grid_dim = (18,18)
 #%% Data setup
 print('Setting up data...')
 # Get list of data
@@ -66,16 +69,29 @@ train_inds = [s not in val_subjs for s in subjs]
 val_IDs = list(compress(IDs,val_inds))
 train_IDs = list(compress(IDs,train_inds))
 
+# load in validation data
+print('Loading validation data')
+x_val = np.zeros((len(val_IDs),*im_dim,5))
+for i,ID in enumerate(val_IDs):
+    im = np.load(data_dir + ID + '.npy')
+    x_val[i] = im
+y_val = np.zeros((len(val_IDs),*grid_dim,7))
+for i,ID in enumerate(val_IDs):
+    # load coordinates
+    coords = np.loadtxt(data_dir + ID + '.txt')
+    # convert coordinates to YOLO
+    target = CoordsToTarget(coords,im_dim,grid_dim)
+    # store
+    y_val[i] = target
+
 #%% Setup model and training
 print('Setting up model...')
 # Generators
-train_gen = DataGenerator(train_IDs, batch_size=32, dim=(256,256),
-                          n_channels=1, shuffle=True,grid_size=(18,18))
-val_gen = DataGenerator(val_IDs, batch_size=32, dim=(256,256),
-                        n_channels=1, shuffle=True,grid_size=(18,18))
+train_gen = DataGenerator(train_IDs, batch_size=32, dim=im_dim,
+                          n_channels=5, shuffle=True,grid_size=grid_dim)
 
 # Build model
-DetectModel = YOLOmodels.BuildInceptionModel((384,384,5))
+DetectModel = YOLOmodels.BuildInceptionModel((*im_dim,5))
 # Compile with custom loss
 opt = keras.optimizers.Adam()
 loss = YOLOmodels.YOLOloss
@@ -86,9 +102,9 @@ DetectModel.compile(opt,loss)
 #%% Train model on dataset
 print('Starting training...')
 DetectModel.fit_generator(generator=train_gen,
-                    validation_data=val_gen,
+                    validation_data=(x_val,y_val),
                     steps_per_epoch=50,
                     validation_steps=13,
                     epochs=2,
                     use_multiprocessing=True,
-                    workers=6)
+                    workers=8)
